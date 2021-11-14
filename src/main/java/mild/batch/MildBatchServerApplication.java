0a434888848401d48ae1b73fae004b72acb19559
kytsaaa6 <kytsaaa6@naver.com>
2021-11-15 03:38:44 +0900

    Job -> Step -> Tasklet 테스트

--- src/main/java/mild/batch/MildBatchServerApplication.java
+++ src/main/java/mild/batch/MildBatchServerApplication.java
@@ -1,9 +1,11 @@
 package mild.batch;
 
+import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@EnableBatchProcessing  // Batch 활성화
 public class MildBatchServerApplication {
 
     public static void main(String[] args) {
