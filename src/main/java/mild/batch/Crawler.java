1c4ef714e115d3034da460efa8be31e6208c64f3
kytsaaa6 <kytsaaa6@naver.com>
2021-12-12 21:12:38 +0900

    Crawler Add Select(title, singer, rank, album, song_img)

--- src/main/java/mild/batch/Crawler.java
+++ src/main/java/mild/batch/Crawler.java
@@ -1,5 +1,6 @@
 package mild.batch;
 
+import mild.batch.data.dto.SongDto;
 import org.jsoup.Connection;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
@@ -7,6 +8,8 @@ import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Crawler {
     public static void main(String[] args) {
@@ -18,19 +21,42 @@ public class Crawler {
             Connection conn = Jsoup.connect(URL);
 
             // 3. HTML 파싱
-            Document html = conn.get();
+            Document doc = conn.get();
 
             // 4. HTML 출력
             //System.out.println(html.toString());
 
+            List<SongDto> songList = new ArrayList<SongDto>();
+
             // 4. 요소 탐색
             // 4-1. 곡 리스트 탐색
-            System.out.println("[Attribute 탐색]");
-            Elements songInfos = html.getElementsByClass("song_info");
-            for(Element songInfo : songInfos) {
-                System.out.println(songInfo.attr("title"));
+            Elements titles = doc.select("div[class=ellipsis rank01]").select("a");
+            Elements singer = doc.select("div[class=ellipsis rank02]").select("a");
+            Elements rank   = doc.select("span[class=rank]");
+            Elements album  = doc.select("div[class=ellipsis rank03]").select("a");
+            Elements songImg  = doc.select("a[class=image_typeAll]").select("img");
 
+            rank.remove(0);
 
+            System.out.println("곡 수 : " + titles.size());
+
+            for(int i=0; i<titles.size(); i++) {
+
+                SongDto song = new SongDto(
+                        titles.get(i).text(),
+                        Integer.parseInt(rank.get(i).text()),
+                        singer.get(i).text(),
+                        album.get(i).text(),
+                        "",
+                        songImg.get(i).attr("src"),
+                        "");
+
+                songList.add(song);
+
+                System.out.println(rank.get(i).text() + "\t" + titles.get(i).text() + "\t" + singer.get(i).text() + "\t" + album.get(i).text());
+
+            }
+            System.out.println(songList.toString());
 /*
                 Elements songs = songInfo.getElementsByTag("a");
                 for(Element elm : files) {
@@ -40,7 +66,6 @@ public class Crawler {
                     System.out.println(text+" > "+href);
                 }
                 */
-            }
 /*
 
             // 4-2. CSS Selector 탐색
